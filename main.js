dac8af64631d407791c50d501ed903d1cf2b7b4c
Apple <apple@Apples-MacBook-Pro-94.local>
2019-06-21 15:19:51 -0700

    number 2

--- main.js
+++ main.js
@@ -20,7 +20,11 @@ function submit(){
 
   var pic = [
     "https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg",
-    "https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg","https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg","https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg","https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg","https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg"
+    "https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg",
+    "https://files.slack.com/files-pri/TK5GC0Y6T-FKSPSUYHK/q2.jpg",
+    "https://images.pexels.com/photos/432722/pexels-photo-432722.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/432722/pexels-photo-432722.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "https://images.pexels.com/photos/432722/pexels-photo-432722.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260"
   ]
   var source = $('.pro')
   $('.pro').attr("src", pic[randomNumber]);
